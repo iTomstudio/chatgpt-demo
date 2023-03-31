1650b322cb52f63d2331e9a1b99d6695b324d1a2
Jerry <43725571+iTomstudio@users.noreply.github.com>
2023-03-31 14:28:00 +0800

    修改密码逻辑

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -4,9 +4,13 @@ const realPassword = import.meta.env.SITE_PASSWORD
 
 export const post: APIRoute = async(context) => {
   const body = await context.request.json()
+  const today = new Date()
+  const dateString = `${today.getFullYear()}${today.getMonth() + 1}${today.getDate()}19961024`
+  const base64encoded = btoa(dateString);
 
+  
   const { pass } = body
   return new Response(JSON.stringify({
-    code: (!realPassword || pass === realPassword) ? 0 : -1,
+    code: (!base64encoded || pass === base64encoded) ? 0 : -1,
   }))
 }
